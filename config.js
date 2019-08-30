e0323d06b5e27fc80369ca6edb284cd74f25b695
al3amri <49554966+al3amri@users.noreply.github.com>
2019-08-31 01:48:48 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NjE3MTEyMzg3MjQ5NjM1MzM1.XWmY3w.mfj-FcidVTzNx72x_rAuqHH6DpQ', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: '/',
+    devs: ['323133445352194048']
+}
